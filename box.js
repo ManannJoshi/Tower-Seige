4746f6138278e048ac71e6c582db29b0684413d7
ManannJoshi <78347709+ManannJoshi@users.noreply.github.com>
2021-05-06 13:11:47 +0530

    Update box.js

--- box.js
+++ box.js
@@ -2,7 +2,7 @@ class Box{
     constructor(x, y, width, height) {
         var options = {
             'restitution':0.4,
-            'friction':0
+            'friction':1
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
@@ -17,6 +17,7 @@ class Box{
         strokeWeight(2);
         stroke(0);
         rect(0, 0, this.width, this.height);
+        rotate(this.body.angle)
         pop();
       }
 }
